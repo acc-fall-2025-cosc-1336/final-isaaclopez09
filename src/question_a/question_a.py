a79539f7a0c41a53587159e351880ae24f2ca652
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-12-10 07:01:06 +0000

    table functions

--- src/question_a/question_a.py
+++ src/question_a/question_a.py
@@ -1,3 +1,16 @@
 #write functions here, don't add input('') statements here!
-def test_config():
-    return True
+
+def create_multiplication_table():
+    table = []
+    for row in range(1,6):
+        row_list = []
+        for col in range(1, 11):
+            row_list.append(row*col)
+        table.append(row_list)
+    return table
+
+def display_multiplication_table(table):
+    for row in table:
+        for value in row:
+            print(f"{value:3}", end="")
+        print()
