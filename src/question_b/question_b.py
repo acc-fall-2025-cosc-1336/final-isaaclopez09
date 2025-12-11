36eead46950cde0a57bc1ab5e7b927dd2c7e1399
isaaclopez09 <mauro.lopez1@g.austincc.edu>
2025-12-11 05:15:26 +0000

    stock class and get stock list function

--- src/question_b/question_b.py
+++ src/question_b/question_b.py
@@ -1,1 +1,21 @@
 #write functions here, don't add input('') statements here!
+class stock:
+    def __init__(self, symbol, company_name):
+        self.__symbol = symbol
+        self.__company_name = company_name
+
+    def get_symbol(self):
+        return self.__symbol
+    
+    def get_company_name(self):
+        return self.__company_name
+    
+def get_stock_list():
+    stock_list = [
+        stock("AAPL", "Apple Inc"),
+        stock("CAT", "Caterpillar"),
+        stock("EK", "Eastman Kodak"),
+        stock("GOOG", "Google"),
+        stock("MSFT", "Microsoft")
+    ]
+    return stock_list
